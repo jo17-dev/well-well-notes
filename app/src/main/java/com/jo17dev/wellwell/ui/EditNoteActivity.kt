8d37c005dafb593c5558183e049c464724edd22b
jo17-dev <procompter1@gmail.com>
2025-08-22 23:45:19 -0400

    designed add & edit note page

---
+++ app/src/main/java/com/jo17dev/wellwell/ui/EditNoteActivity.kt
@@ -0,0 +1,21 @@
+package com.jo17dev.wellwell.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.jo17dev.wellwell.R
+
+class EditNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_edit_note)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
